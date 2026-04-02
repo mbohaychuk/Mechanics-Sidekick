6d853895fa8da39d1b5293aa800c458c77ed45c9
Mark Bohaychuk <mark@bohaychuk.com>
2026-04-02 10:36:25 -0600

    feat: add database engine and session factory

---
+++ app/db.py
@@ -0,0 +1,15 @@
+# app/db.py
+from sqlalchemy import create_engine, Engine
+from sqlalchemy.orm import sessionmaker, Session, DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+def get_engine(db_url: str) -> Engine:
+    return create_engine(db_url, connect_args={"check_same_thread": False})
+
+
+def get_session_factory(engine: Engine):
+    return sessionmaker(bind=engine, expire_on_commit=False)
